5b1500bedf5ceefc651483667a9ffca34b668d6d
Baskaran Shanmugam <baskaran.shanmugam@mindtree.com>
2017-05-09 10:31:19 +0530

    updates

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ exports.home=function(req,res){
   res.render('index',{
     sitename:'I Love My City',
     title:'I Love My City',
-    headline:'Arunkumar - module 2 assignment using ejs',
+    headline:'Baskaran - module 2 assignment using ejs',
   });
 }
 
